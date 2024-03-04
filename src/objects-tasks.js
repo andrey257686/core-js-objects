04ff9b7999fb4bc188cc391ca414291d2056d729
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-03-04 21:12:34 +0500

    feat: Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -191,8 +191,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
+  // throw new Error('Not implemented');
 }
 
 /**
